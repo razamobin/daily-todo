8ca8d9b8445a446a365cc1173bbc9e26e9afd6a1
Raza Mobin <rmobin@deepdyve.com>
2024-06-23 11:45:26 -0700

    feat: add db backup script

---
+++ backup_mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set variables
+DB_USER="user"
+DB_PASSWORD="password"
+DB_NAME="todo_db"
+BACKUP_DIR="/Users/rmobin/Dropbox/2024aiagents/daily-todo/dbbackups"  # Change this to your desired backup directory
+DATE=$(date +"%Y%m%d%H%M")
+BACKUP_FILE="$BACKUP_DIR/$DB_NAME-$DATE.sql"
+
+# Create backup using docker exec and passing the password securely
+docker exec -i mysql /bin/bash -c "mysqldump -u$DB_USER -p$DB_PASSWORD $DB_NAME" > $BACKUP_FILE
+
+# Remove backups older than 7 days
+find $BACKUP_DIR -type f -name "$DB_NAME-*.sql" -mtime +7 -exec rm {} \;
