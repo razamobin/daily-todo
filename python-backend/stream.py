4383329972bc2288966cc1fd03adef1a2d510740
Raza Mobin <rmobin@deepdyve.com>
2024-06-27 06:44:34 -0700

    feat: stream.py has the streaming working from the assistant api, nice

--- python-backend/stream.py
+++ python-backend/stream.py
@@ -11,11 +11,19 @@ from openai import Client
 import os
 from typing import List
 from openai.types.beta.function_tool_param import FunctionToolParam
+from openai.types.beta.threads import Message, MessageDelta
+from openai.types.beta.threads.runs import ToolCall
+from openai.types.beta.threads.runs import RunStep
+from openai.types.beta.threads.runs import FunctionToolCallDelta
+from openai.types.beta.assistant_stream_event import AssistantStreamEvent
 
 from pydantic import Field
 from instructor import OpenAISchema
 
 from dotenv import load_dotenv
+
+from typing_extensions import override
+from openai import AssistantEventHandler
 
 
 class GetUserMission(OpenAISchema):
@@ -28,8 +36,7 @@ class GetUserMission(OpenAISchema):
 
     def run(self):
         response = requests.get(
-            f'http://localhost:8080/api/user-mission?user_id={self.user_id}'
-        )
+            f'http://localhost:8080/api/user-mission?user_id={self.user_id}')
         if response.status_code != 200:
             return f"Error: Failed to fetch latest thread ID, Status Code: {response.status_code}"
         else:
@@ -131,6 +138,201 @@ def get_completion(client, message, agent, funcs, thread):
             return message
 
 
+def get_completion_stream(client, message, agent, funcs, thread):
+    # create new message in the thread
+    message = client.beta.threads.messages.create(thread_id=thread.id,
+                                                  role="user",
+                                                  content=message)
+
+    # First, we create a EventHandler class to define
+    # how we want to handle the events in the response stream.
+
+    class EventHandler(AssistantEventHandler):
+
+        def __init__(self, thread_id, assistant_id):
+            super().__init__()
+            self.output = None
+            self.tool_id = None
+            self.thread_id = thread_id
+            self.assistant_id = assistant_id
+            self.run_id = None
+            self.run_step: RunStep | None = None
+            self.function_name = ""
+            self.arguments = ""
+
+        @override
+        def on_text_created(self, text) -> None:
+            print(f"\nassistant on_text_created > ", end="", flush=True)
+
+        @override
+        def on_text_delta(self, delta, snapshot):
+            print(f"\nassistant on_text_delta > {delta.value}",
+                  end="",
+                  flush=True)
+
+        @override
+        def on_end(self, ):
+            print(f"\n end assistant > ",
+                  self.current_run_step_snapshot,
+                  end="",
+                  flush=True)
+
+        @override
+        def on_exception(self, exception: Exception) -> None:
+            """Fired whenever an exception happens during streaming"""
+            print(f"\nassistant > {exception}\n", end="", flush=True)
+
+        @override
+        def on_message_created(self, message: Message) -> None:
+            print(f"\nassistant on_message_created > {message}\n",
+                  end="",
+                  flush=True)
+
+        @override
+        def on_message_done(self, message: Message) -> None:
+            print(f"\nassistant on_message_done > {message}\n",
+                  end="",
+                  flush=True)
+
+        @override
+        def on_message_delta(self, delta: MessageDelta,
+                             snapshot: Message) -> None:
+            # print(f"\nassistant on_message_delta > {delta}\n", end="", flush=True)
+            pass
+
+        def on_tool_call_created(self, tool_call: ToolCall):
+            # 4
+            print(f"\nassistant on_tool_call_created > {tool_call}")
+            self.function_name = tool_call.function.name
+            self.tool_id = tool_call.id
+            print(
+                f"\on_tool_call_created > run_step.status > {self.run_step.status}"
+            )
+
+            print(f"\nassistant > {tool_call.type} {self.function_name}\n",
+                  flush=True)
+
+            keep_retrieving_run = client.beta.threads.runs.retrieve(
+                thread_id=self.thread_id, run_id=self.run_id)
+
+            while keep_retrieving_run.status in ["queued", "in_progress"]:
+                keep_retrieving_run = client.beta.threads.runs.retrieve(
+                    thread_id=self.thread_id, run_id=self.run_id)
+
+                print(f"\nSTATUS: {keep_retrieving_run.status}")
+
+        @override
+        def on_tool_call_done(self, tool_call: ToolCall) -> None:
+            keep_retrieving_run = client.beta.threads.runs.retrieve(
+                thread_id=self.thread_id, run_id=self.run_id)
+
+            print(f"\nDONE STATUS: {keep_retrieving_run.status}")
+
+            if keep_retrieving_run.status == "completed":
+                all_messages = client.beta.threads.messages.list(
+                    thread_id=self.thread_id)
+
+                print(all_messages.data[0].content[0].text.value, "", "")
+                return
+
+            elif keep_retrieving_run.status == "requires_action":
+                print("here you would call your function")
+
+                #function_data = my_example_funtion()
+                # find the tool to be executed
+                func = next(
+                    iter([
+                        func for func in funcs
+                        if func.__name__ == self.function_name
+                    ]))
+
+                try:
+                    # init tool
+                    func = func(**eval(tool_call.function.arguments))
+                    # get outputs from the tool
+                    self.output = func.run()
+                except Exception as e:
+                    self.output = "Error: " + str(e)
+
+                #self.output=function_data
+
+                with client.beta.threads.runs.submit_tool_outputs_stream(
+                        thread_id=self.thread_id,
+                        run_id=self.run_id,
+                        tool_outputs=[{
+                            "tool_call_id": self.tool_id,
+                            "output": self.output,
+                        }],
+                        event_handler=EventHandler(
+                            self.thread_id, self.assistant_id)) as stream:
+                    stream.until_done()
+
+        @override
+        def on_run_step_created(self, run_step: RunStep) -> None:
+            # 2
+            print(f"on_run_step_created")
+            self.run_id = run_step.run_id
+            self.run_step = run_step
+            print("The type ofrun_step run step is ",
+                  type(run_step),
+                  flush=True)
+            print(f"\n run step created assistant > {run_step}\n", flush=True)
+
+        @override
+        def on_run_step_done(self, run_step: RunStep) -> None:
+            print(f"\n run step done assistant > {run_step}\n", flush=True)
+
+        def on_tool_call_delta(self, delta, snapshot):
+            if delta.type == 'function':
+                # the arguments stream thorugh here and then you get the requires action event
+                print(delta.function.arguments, end="", flush=True)
+                self.arguments += delta.function.arguments
+            elif delta.type == 'code_interpreter':
+                print(f"on_tool_call_delta > code_interpreter")
+                if delta.code_interpreter.input:
+                    print(delta.code_interpreter.input, end="", flush=True)
+                if delta.code_interpreter.outputs:
+                    print(f"\n\noutput >", flush=True)
+                    for output in delta.code_interpreter.outputs:
+                        if output.type == "logs":
+                            print(f"\n{output.logs}", flush=True)
+            else:
+                print("ELSE")
+                print(delta, end="", flush=True)
+
+        @override
+        def on_event(self, event: AssistantStreamEvent) -> None:
+            # print("In on_event of event is ", event.event, flush=True)
+
+            if event.event == "thread.run.requires_action":
+                print("\nthread.run.requires_action > submit tool call")
+                print(f"ARGS: {self.arguments}")
+
+
+# Then, we use the `stream` SDK helper
+# with the `EventHandler` class to create the Run
+# and stream the response.
+
+# run this thread
+
+    with client.beta.threads.runs.stream(
+            thread_id=thread.id,
+            assistant_id=agent.id,
+            event_handler=EventHandler(thread_id=thread.id,
+                                       assistant_id=agent.id),
+    ) as stream:
+        stream.until_done()
+"""
+    yield "Starting stream...\n"
+    time.sleep(3)
+    yield "Encouraging message...\n"
+    time.sleep(2)
+    yield "Todo history...\n"
+    time.sleep(1)
+    yield "Stream ended.\n"
+"""
+
+
 def generate_messages():
     yield "Starting stream...\n"
     for i in range(5):
@@ -225,6 +427,7 @@ def daily_message():
 
     return Response(generate(), content_type='text/event-stream')
 
+
 if __name__ == '__main__':
     print('hello world')
     user_id: int = 1
@@ -260,7 +463,7 @@ if __name__ == '__main__':
     # add a message to the thread "send an encouraging message"
     eternal_optimist_tools = [GetUserMission]
 
-    message_generator = get_completion(
+    message_generator = get_completion_stream(
         client=client,
         message=
         f"send an encouraging message to user {user_id}. use tools to get the user's mission and recent daily todo history :)",
@@ -268,8 +471,8 @@ if __name__ == '__main__':
         funcs=eternal_optimist_tools,
         thread=thread)
 
-    print(message_generator)
-
+    for message in message_generator:
+        wprint(message)
 """
     @stream_with_context
     def generate():
