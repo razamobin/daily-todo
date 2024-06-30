c7cedb5392c14158aff111c44fc9022632a4db96
Raza Mobin <rmobin@deepdyve.com>
2024-06-30 11:29:04 -0700

    fix: use hello pangea dnd which keeps beautiful dnd up to date

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 
 function TodoList({ todos, setTodos }) {
     const handleQuantityChange = (todo, index) => {
